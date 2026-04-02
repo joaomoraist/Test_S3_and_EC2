5a9959ec16be2a0e141ba7681cd11b72d24edecf
João Morais <jv.timotio@gmail.com>
2026-04-02 15:42:03 -0300

    Create controller

--- src/main/java/br/com/teste/testeAws/controller/s3Controller.java
+++ src/main/java/br/com/teste/testeAws/controller/s3Controller.java
@@ -7,5 +7,18 @@ import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.s3.S3Client;
 import software.amazon.awssdk.services.s3.model.PutObjectRequest;
 
+
+@RestController
+@RequestMapping
 public class s3Controller {
+
+    private final S3Client s3 = S3Client.builder().region(Region.US_EAST_1).build();
+    private final String BUCKET = "estudo-teste";
+
+    @PostMapping("/upload")
+    public String upload(@RequestParam("file") MultipartFile file) throws Exception {
+        s3.putObject(PutObjectRequest.builder().bucket(BUCKET).key(file.getOriginalFilename()).build(),
+                RequestBody.fromInputStream(file.getInputStream(), file.getSize()));
+        return "Arquivo enviado!";
+    }
 }
