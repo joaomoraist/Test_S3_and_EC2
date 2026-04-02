099cc92840fb11fd9a69bfa87d72f33c024342a5
João Morais <jv.timotio@gmail.com>
2026-04-02 15:11:55 -0300

    Initial setup project

---
+++ src/main/java/br/com/teste/testeAws/TesteAwsApplication.java
@@ -0,0 +1,13 @@
+package br.com.teste.testeAws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TesteAwsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TesteAwsApplication.class, args);
+	}
+
+}
---
+++ src/main/java/br/com/teste/testeAws/controller/s3Controller.java
@@ -0,0 +1,11 @@
+package br.com.teste.testeAws.controller;
+
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+import software.amazon.awssdk.core.sync.RequestBody;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.model.PutObjectRequest;
+
+public class s3Controller {
+}
